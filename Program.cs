ea9e5334434fe8799b0ffebfd7467ed7eed3b200
ArmyAntMF54 <anthonyjpadgett@gmail.com>
2021-04-11 12:10:26 -0500

    Task Done

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SyntaxSugarTC2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var answer = 3;
+            //string response;
+
+
+            //if (answer < 12)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + "greater than or equal to nine";
+            //}
+
+            var answer = 3;
+            var response = (answer < 12) ? $"{answer} is less than twelve" : $"{answer} is greater than or equal to twelve";
+            Console.WriteLine(response);
+        }
+
+    }
+}
